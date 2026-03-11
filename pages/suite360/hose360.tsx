949b387aa87c09a474d1557f16ed3382e0cf4a7a
WahWahKhan <wahwahkhan@github.com>
2026-03-11 17:04:25 +1100

    Fixed Hose360 tag

--- pages/suite360/hose360.tsx
+++ pages/suite360/hose360.tsx
@@ -51,7 +51,7 @@ const HoseBuilder = () => {
   return (
     <>
       <Head>
-        <title>Hose Builder - Fluid Power Group</title>
+        <title>Hose360 - Fluid Power Group</title>
         <meta name="description" content="Build your own custom hydraulic hose online. Select hose size, end fittings, cut length and quantity. Done in under 5 minutes. Order direct from FluidPower Group." />
         <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1.00001,viewport-fit=cover" />
         
